804ac3b07130fbe30f51435c7fbca765fbf039e4
fanatic <fanatic@g>
2020-04-06 22:16:12 +0530

    Added runner and testng

---
+++ com.org.api/src/test/java/APITesting/com/org/api/utils/RunnerClass.java
@@ -0,0 +1,50 @@
+package APITesting.com.org.api.utils;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.cli.*;
+import org.testng.TestNG;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
+import org.xml.sax.SAXException;
+import org.testng.xml.Parser; 
+public class RunnerClass {
+	
+	
+	
+
+		public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException  {
+			// TODO Auto-generated method stub
+		
+				
+				TestNG suit = new TestNG();
+				
+		        final Parser parser = new Parser("E:\\MY-REP\\com.org.api\\src\\test\\java\\APITesting\\com\\org\\api\\utils\\testng.xml");
+		        final List<XmlSuite> suites = parser.parseToList();
+		        System.out.println("set suites" );
+		        suit.setXmlSuites(suites);
+		        System.out.println("run" );
+		        suit.run();
+		
+
+	        
+	    }
+
+			
+			
+			
+			
+
+		
+
+	}
+
+
+
